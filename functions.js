57eb67c5bcee7dea7cd6e1767ac41f3023144f1f
Brian Cheung <brian.cheung1@baruchmail.cuny.edu>
2021-03-06 23:22:44 -0500

    added some animations

--- functions.js
+++ functions.js
@@ -5,3 +5,25 @@ function copyToClipboard(element) {
     document.execCommand("copy");
     $temp.remove();
   }
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("scroll", function(event) {
+        const animatedBoxes = document.getElementsByClassName("title-anim");
+        const windowOffsetTop = window.innerHeight + window.scrollY;
+
+        Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+            const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+            if (windowOffsetTop >= animatedBoxOffsetTop) {
+                addClass(animatedBox, "fade-in");
+            }
+        });
+    });
+});
+
+function addClass(element, className) {
+    const arrayClasses = element.className.split(" ");
+    if (arrayClasses.indexOf(className) === -1) {
+        element.className += " " + className;
+    }
+}
